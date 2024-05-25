e50da95664e71ecd9dc0bf4714b2c30f52fd4605
erensglm <101216457+erensglm@users.noreply.github.com>
2024-05-25 22:50:21 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kategori Uygulaması',
+      title: 'Yemek Kategori Uygulaması',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
